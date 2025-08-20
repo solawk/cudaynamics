b8adf264eaf130ee0733c57579d6703b96c05ccb
TheSnowMan <134663057+thesnowbrah@users.noreply.github.com>
2025-08-20 18:34:38 +0300

    Add files via upload

--- main.h
+++ main.h
@@ -28,8 +28,8 @@
 #include "kernels/sang/sang.h"
 #include "kernels/rabinovich_fabrikant/rabinovich_fabrikant.h"
 #include "kernels/thomas/thomas.h"
+#include "kernels/bolshakov/bolshakov.h"
 #include "kernels/vnm/vnm.h"
-#include "kernels/fitznagumo/fitznagumo.h"
 
 extern std::map<std::string, Kernel> kernels;
 extern std::map<std::string, int> kernelTPBs;
