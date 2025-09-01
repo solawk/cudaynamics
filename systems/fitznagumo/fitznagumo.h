694fc3404f59a971d981df069b66bf2aad384fe6
Александр Ханов <fulmenius@yandex.ru>
2025-09-01 20:35:17 +0300

    systems renamings

--- systems/fitznagumo/fitznagumo.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <kernels_common.h>
-
-const int THREADS_PER_BLOCK_fitznagumo = 64;
-
-__global__ void kernelProgram_fitznagumo(Computation* data);
-
-__device__ void finiteDifferenceScheme_fitznagumo(numb* currentV, numb* nextV, numb* parameters);
