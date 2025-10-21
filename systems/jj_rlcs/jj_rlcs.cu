35335e156fe4ece53b143c8594b4db697d0c09e4
TheSnowMan <134663057+thesnowbrah@users.noreply.github.com>
2025-10-21 22:49:18 +0300

    Add files via upload

--- systems/jj_rlcs/jj_rlcs.cu
+++ systems/jj_rlcs/jj_rlcs.cu
@@ -4,7 +4,7 @@
 namespace attributes
 {
     enum variables { x1, sin_x1, x2, x3 };
-    enum parameters { betaL, betaC, i, gThreshold, Rn, Rsg, stepsize, symmetry, method };
+    enum parameters { betaL, betaC, i, gThreshold, Rn, Rsg, symmetry, method };
     enum methods { ExplicitEuler, SemiExplicitEuler, ExplicitMidpoint, ExplicitRungeKutta4, VariableSymmetryCD };
     enum maps { LLE, MAX, MeanInterval, MeanPeak, Period };
 }
@@ -46,8 +46,8 @@ __global__ void kernelProgram_jj_rlcs(Computation* data)
     }
     if (M(Period).toCompute || M(MeanInterval).toCompute || M(MeanPeak).toCompute)
     {
-        DBscan_Settings dbscan_settings(MS(Period, 0), MS(MeanInterval, 0), MS(Period, 1), MS(Period, 2), MS(MeanInterval, 1), MS(MeanInterval, 2), MS(MeanInterval, 3), MS(MeanInterval, 4), P(stepsize));
-        Period(data, dbscan_settings, variation, &finiteDifferenceScheme_jj_rlcs, MO(Period), MO(MeanPeak), MO(MeanInterval));
+        DBscan_Settings dbscan_settings(MS(Period, 0), MS(MeanInterval, 0), MS(Period, 1), MS(Period, 2), MS(MeanInterval, 1), MS(MeanInterval, 2), MS(MeanInterval, 3), MS(MeanInterval, 4),
+            H_BRANCH(parameters[CUDA_kernel.PARAM_COUNT - 1], variables[CUDA_kernel.VAR_COUNT - 1]));
     }
 }
 
@@ -55,30 +55,30 @@ __device__ __forceinline__ void finiteDifferenceScheme_jj_rlcs(numb* currentV, n
 {
     ifMETHOD(P(method), ExplicitEuler)
     {
-        Vnext(x1) = fmodf(V(x1) + P(stepsize) * V(x2), 2.0f * 3.141592f);
+        Vnext(x1) = fmodf(V(x1) + H * V(x2), 2.0f * 3.141592f);
         Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x2) = V(x2) + P(stepsize) * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
-        Vnext(x3) = V(x3) + P(stepsize) * (1.0f / P(betaL)) * (V(x2) - V(x3));
+        Vnext(x2) = V(x2) + H * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
+        Vnext(x3) = V(x3) + H * (1.0f / P(betaL)) * (V(x2) - V(x3));
     }
 
     ifMETHOD(P(method), SemiExplicitEuler)
     {
-        Vnext(x1) = fmodf(V(x1) + P(stepsize) * V(x2), 2.0f * 3.141592f);
+        Vnext(x1) = fmodf(V(x1) + H * V(x2), 2.0f * 3.141592f);
         Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x3) = V(x3) + P(stepsize) * (1.0f / P(betaL)) * (V(x2) - V(x3));
-        Vnext(x2) = V(x2) + P(stepsize) * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(Vnext(x1)) - Vnext(x3));
+        Vnext(x3) = V(x3) + H * (1.0f / P(betaL)) * (V(x2) - V(x3));
+        Vnext(x2) = V(x2) + H * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(Vnext(x1)) - Vnext(x3));
     }
 
     ifMETHOD(P(method), ExplicitMidpoint)
     {
-        numb x1mp = fmodf(V(x1) + P(stepsize) * 0.5 * V(x2), 2.0f * 3.141592f);
-        numb x2mp = V(x2) + P(stepsize) * 0.5 * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
-        numb x3mp = V(x3) + P(stepsize) * 0.5 * (1.0f / P(betaL)) * (V(x2) - V(x3));
+        numb x1mp = fmodf(V(x1) + H * 0.5 * V(x2), 2.0f * 3.141592f);
+        numb x2mp = V(x2) + H * 0.5 * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
+        numb x3mp = V(x3) + H * 0.5 * (1.0f / P(betaL)) * (V(x2) - V(x3));
 
-        Vnext(x1) = fmodf(V(x1) + P(stepsize) * x2mp, 2.0f * 3.141592f);
+        Vnext(x1) = fmodf(V(x1) + H * x2mp, 2.0f * 3.141592f);
         Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x2) = V(x2) + P(stepsize) * (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
-        Vnext(x3) = V(x3) + P(stepsize) * (1.0f / P(betaL)) * (x2mp - x3mp);
+        Vnext(x2) = V(x2) + H * (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
+        Vnext(x3) = V(x3) + H * (1.0f / P(betaL)) * (x2mp - x3mp);
     }
 
     ifMETHOD(P(method), ExplicitRungeKutta4)
@@ -87,40 +87,40 @@ __device__ __forceinline__ void finiteDifferenceScheme_jj_rlcs(numb* currentV, n
         numb kx21 = (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
         numb kx31 = (1.0f / P(betaL)) * (V(x2) - V(x3));
 
-        numb x1mp = fmodf(V(x1) + P(stepsize) * 0.5 * kx11, 2.0f * 3.141592f);
-        numb x2mp = V(x2) + P(stepsize) * 0.5 * kx21;
-        numb x3mp = V(x3) + P(stepsize) * 0.5 * kx31;
+        numb x1mp = fmodf(V(x1) + H * 0.5 * kx11, 2.0f * 3.141592f);
+        numb x2mp = V(x2) + H * 0.5 * kx21;
+        numb x3mp = V(x3) + H * 0.5 * kx31;
 
         numb kx12 = x2mp;
         numb kx22 = (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
         numb kx32 = (1.0f / P(betaL)) * (x2mp - x3mp);
 
-        x1mp = fmodf(V(x1) + P(stepsize) * 0.5 * kx12, 2.0f * 3.141592f);
-        x2mp = V(x2) + P(stepsize) * 0.5 * kx22;
-        x3mp = V(x3) + P(stepsize) * 0.5 * kx32;
+        x1mp = fmodf(V(x1) + H * 0.5 * kx12, 2.0f * 3.141592f);
+        x2mp = V(x2) + H * 0.5 * kx22;
+        x3mp = V(x3) + H * 0.5 * kx32;
 
         numb kx13 = x2mp;
         numb kx23 = (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
         numb kx33 = (1.0f / P(betaL)) * (x2mp - x3mp);
 
-        x1mp = fmodf(V(x1) + P(stepsize) * kx13, 2.0f * 3.141592f);
-        x2mp = V(x2) + P(stepsize) * kx23;
-        x3mp = V(x3) + P(stepsize) * kx33;
+        x1mp = fmodf(V(x1) + H * kx13, 2.0f * 3.141592f);
+        x2mp = V(x2) + H * kx23;
+        x3mp = V(x3) + H * kx33;
 
         numb kx14 = x2mp;
         numb kx24 = (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
         numb kx34 = (1.0f / P(betaL)) * (x2mp - x3mp);
 
-        Vnext(x1) = fmodf(V(x1) + P(stepsize) * (kx11 + 2 * kx12 + 2 * kx13 + kx14) / 6, 2.0f * 3.141592f);
+        Vnext(x1) = fmodf(V(x1) + H * (kx11 + 2 * kx12 + 2 * kx13 + kx14) / 6, 2.0f * 3.141592f);
         Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x2) = V(x2) + P(stepsize) * (kx21 + 2 * kx22 + 2 * kx23 + kx24) / 6;
-        Vnext(x3) = V(x3) + P(stepsize) * (kx31 + 2 * kx32 + 2 * kx33 + kx34) / 6;
+        Vnext(x2) = V(x2) + H * (kx21 + 2 * kx22 + 2 * kx23 + kx24) / 6;
+        Vnext(x3) = V(x3) + H * (kx31 + 2 * kx32 + 2 * kx33 + kx34) / 6;
     }
 
     ifMETHOD(P(method), VariableSymmetryCD)
     {
-        numb h1 = 0.5 * P(stepsize) - P(symmetry);
-        numb h2 = 0.5 * P(stepsize) + P(symmetry);
+        numb h1 = 0.5 * H - P(symmetry);
+        numb h2 = 0.5 * H + P(symmetry);
 
         numb x1mp = fmodf(V(x1) + h1 * V(x2), 2.0f * 3.141592f);
         numb x3mp = V(x3) + h1 * (1.0f / P(betaL)) * (V(x2) - V(x3));
