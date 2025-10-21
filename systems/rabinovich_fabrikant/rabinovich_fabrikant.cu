a75b5b8f61515c99a41ceb0250c4d72b51aeaf2d
TheSnowMan <134663057+thesnowbrah@users.noreply.github.com>
2025-10-21 22:51:00 +0300

    Add files via upload

--- systems/rabinovich_fabrikant/rabinovich_fabrikant.cu
+++ systems/rabinovich_fabrikant/rabinovich_fabrikant.cu
@@ -4,7 +4,7 @@
 namespace attributes
 {
     enum variables { x, y, z };
-    enum parameters { alpha, betta, gamma, delta, epsilon, stepsize, symmetry, method };
+    enum parameters { alpha, betta, gamma, delta, epsilon, symmetry, method };
     enum methods { ExplicitEuler, ExplicitMidpoint, ExplicitRungeKutta4, VariableSymmetryCD};
     enum maps { LLE, MAX, MeanInterval, MeanPeak, Period };
 }
@@ -47,7 +47,8 @@ __global__ void kernelProgram_rabinovich_fabrikant(Computation* data)
 
     if (M(Period).toCompute || M(MeanInterval).toCompute || M(MeanPeak).toCompute)
     {
-        DBscan_Settings dbscan_settings(MS(Period, 0), MS(MeanInterval, 0), MS(Period, 1), MS(Period, 2), MS(MeanInterval, 1), MS(MeanInterval, 2), MS(MeanInterval, 3), MS(MeanInterval, 4), P(stepsize));
+        DBscan_Settings dbscan_settings(MS(Period, 0), MS(MeanInterval, 0), MS(Period, 1), MS(Period, 2), MS(MeanInterval, 1), MS(MeanInterval, 2), MS(MeanInterval, 3), MS(MeanInterval, 4),
+            H_BRANCH(parameters[CUDA_kernel.PARAM_COUNT - 1], variables[CUDA_kernel.VAR_COUNT - 1]));
         Period(data, dbscan_settings, variation, &finiteDifferenceScheme_rabinovich_fabrikant, MO(Period), MO(MeanPeak), MO(MeanInterval));
     }
 }
@@ -56,20 +57,20 @@ __device__ __forceinline__ void finiteDifferenceScheme_rabinovich_fabrikant(numb
 {
     ifMETHOD(P(method), ExplicitEuler)
     {
-        Vnext(x) = V(x) + P(stepsize) * (V(y) * (V(z) - P(betta) + pow(V(x), 2)) + P(gamma) * V(x));
-        Vnext(y) = V(y) + P(stepsize) * (V(x) * (P(delta) * V(z) + P(betta) - pow(V(x), 2)) + P(gamma) * V(y));
-        Vnext(z) = V(z) + P(stepsize) * (P(epsilon) * V(z) * (P(alpha) + V(x) * V(y)));
+        Vnext(x) = V(x) + H * (V(y) * (V(z) - P(betta) + pow(V(x), 2)) + P(gamma) * V(x));
+        Vnext(y) = V(y) + H * (V(x) * (P(delta) * V(z) + P(betta) - pow(V(x), 2)) + P(gamma) * V(y));
+        Vnext(z) = V(z) + H * (P(epsilon) * V(z) * (P(alpha) + V(x) * V(y)));
     }
 
     ifMETHOD(P(method), ExplicitMidpoint)
     {
-        numb xmp = V(x) + P(stepsize) * 0.5 * (V(y) * (V(z) - P(betta) + pow(V(x), 2)) + P(gamma) * V(x));
-        numb ymp = V(y) + P(stepsize) * 0.5 * (V(x) * (P(delta) * V(z) + P(betta) - pow(V(x), 2)) + P(gamma) * V(y));
-        numb zmp = V(z) + P(stepsize) * 0.5 * (P(epsilon) * V(z) * (P(alpha) + V(x) * V(y)));
+        numb xmp = V(x) + H * 0.5 * (V(y) * (V(z) - P(betta) + pow(V(x), 2)) + P(gamma) * V(x));
+        numb ymp = V(y) + H * 0.5 * (V(x) * (P(delta) * V(z) + P(betta) - pow(V(x), 2)) + P(gamma) * V(y));
+        numb zmp = V(z) + H * 0.5 * (P(epsilon) * V(z) * (P(alpha) + V(x) * V(y)));
 
-        Vnext(x) = V(x) + P(stepsize) * (ymp * (zmp - P(betta) + pow(xmp, 2)) + P(gamma) * xmp);
-        Vnext(y) = V(y) + P(stepsize) * (xmp * (P(delta) * zmp + P(betta) - pow(xmp, 2)) + P(gamma) * ymp);
-        Vnext(z) = V(z) + P(stepsize) * (P(epsilon) * zmp * (P(alpha) + xmp * ymp));
+        Vnext(x) = V(x) + H * (ymp * (zmp - P(betta) + pow(xmp, 2)) + P(gamma) * xmp);
+        Vnext(y) = V(y) + H * (xmp * (P(delta) * zmp + P(betta) - pow(xmp, 2)) + P(gamma) * ymp);
+        Vnext(z) = V(z) + H * (P(epsilon) * zmp * (P(alpha) + xmp * ymp));
     }
 
     ifMETHOD(P(method), ExplicitRungeKutta4)
@@ -78,39 +79,39 @@ __device__ __forceinline__ void finiteDifferenceScheme_rabinovich_fabrikant(numb
         numb ky1 = V(x) * (P(delta) * V(z) + P(betta) - pow(V(x), 2)) + P(gamma) * V(y);
         numb kz1 = P(epsilon) * V(z) * (P(alpha) + V(x) * V(y));
 
-        numb xmp = V(x) + 0.5 * P(stepsize) * kx1;
-        numb ymp = V(y) + 0.5 * P(stepsize) * ky1;
-        numb zmp = V(z) + 0.5 * P(stepsize) * kz1;
+        numb xmp = V(x) + 0.5 * H * kx1;
+        numb ymp = V(y) + 0.5 * H * ky1;
+        numb zmp = V(z) + 0.5 * H * kz1;
 
         numb kx2 = ymp * (zmp - P(betta) + pow(xmp, 2)) + P(gamma) * xmp;
         numb ky2 = xmp * (P(delta) * zmp + P(betta) - pow(xmp, 2)) + P(gamma) * ymp;
         numb kz2 = P(epsilon) * zmp * (P(alpha) + xmp * ymp);
 
-        xmp = V(x) + 0.5 * P(stepsize) * kx2;
-        ymp = V(y) + 0.5 * P(stepsize) * ky2;
-        zmp = V(z) + 0.5 * P(stepsize) * kz2;
+        xmp = V(x) + 0.5 * H * kx2;
+        ymp = V(y) + 0.5 * H * ky2;
+        zmp = V(z) + 0.5 * H * kz2;
 
         numb kx3 = ymp * (zmp - P(betta) + pow(xmp, 2)) + P(gamma) * xmp;
         numb ky3 = xmp * (P(delta) * zmp + P(betta) - pow(xmp, 2)) + P(gamma) * ymp;
         numb kz3 = P(epsilon) * zmp * (P(alpha) + xmp * ymp);
 
-        xmp = V(x) + P(stepsize) * kx3;
-        ymp = V(y) + P(stepsize) * ky3;
-        zmp = V(z) + P(stepsize) * kz3;
+        xmp = V(x) + H * kx3;
+        ymp = V(y) + H * ky3;
+        zmp = V(z) + H * kz3;
 
         numb kx4 = ymp * (zmp - P(betta) + pow(xmp, 2)) + P(gamma) * xmp;
         numb ky4 = xmp * (P(delta) * zmp + P(betta) - pow(xmp, 2)) + P(gamma) * ymp;
         numb kz4 = P(epsilon) * zmp * (P(alpha) + xmp * ymp);
 
-        Vnext(x) = V(x) + P(stepsize) * (kx1 + 2.0 * kx2 + 2.0 * kx3 + kx4) / 6.0;
-        Vnext(y) = V(y) + P(stepsize) * (ky1 + 2.0 * ky2 + 2.0 * ky3 + ky4) / 6.0;
-        Vnext(z) = V(z) + P(stepsize) * (kz1 + 2.0 * kz2 + 2.0 * kz3 + kz4) / 6.0;
+        Vnext(x) = V(x) + H * (kx1 + 2.0 * kx2 + 2.0 * kx3 + kx4) / 6.0;
+        Vnext(y) = V(y) + H * (ky1 + 2.0 * ky2 + 2.0 * ky3 + ky4) / 6.0;
+        Vnext(z) = V(z) + H * (kz1 + 2.0 * kz2 + 2.0 * kz3 + kz4) / 6.0;
     }
 
     ifMETHOD(P(method), VariableSymmetryCD)
     {
-        numb h1 = 0.5 * P(stepsize) - P(symmetry);
-        numb h2 = 0.5 * P(stepsize) + P(symmetry);
+        numb h1 = 0.5 * H - P(symmetry);
+        numb h2 = 0.5 * H + P(symmetry);
 
         numb xmp = V(x) + h1 * (V(y) * (V(z) - P(betta) + pow(V(x), 2)) + P(gamma) * V(x));
         numb ymp = V(y) + h1 * (xmp * (P(delta) * V(z) + P(betta) - pow(xmp, 2)) + P(gamma) * V(y));
