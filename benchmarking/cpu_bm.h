4b305bdad26e742ea75d54b447eee225b54e64fb
Alexander <fulmenius@yandex.ru>
2025-11-22 14:25:43 +0300

    removed benchmark

--- benchmarking/cpu_bm.h
+++ benchmarking/cpu_bm.h
@@ -1,8 +1,1 @@
 #pragma once
-#include "main.h"
-#include "cuda_macros.h"
-#include <omp.h>
-
-void AnalysisLobby_cpu(Computation* data, void(*finiteDifferenceScheme)(numb*, numb*, numb*), int variation);
-
-void cpu_execute(Computation* data, bool openmp);
