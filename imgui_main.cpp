fb50394048c17202aece13ffda82852e6fa79992
Alexander <fulmenius@yandex.ru>
2025-12-17 10:32:42 +0300

    indices

--- imgui_main.cpp
+++ imgui_main.cpp
@@ -112,7 +112,7 @@ std::string rangingDescriptions[] =
     "Uniform random distribution of values between 'min' and 'max'",
     "Normal random distribution of values around 'mu' with standard deviation 'sigma'"
 };
-std::string plottypes[] = { "Variables time series", "3D Phase diagram", "2D Phase diagram", "Orbit diagram", "Heatmap", "RGB Heatmap", "Indeces diagram", "Indeces time series", "Decay plot"};
+std::string plottypes[] = { "Variables time series", "3D Phase diagram", "2D Phase diagram", "Orbit diagram", "Heatmap", "RGB Heatmap", "Indices diagram", "Indices time series", "Decay plot"};
 
 bool rangingWindowEnabled = true;
 bool graphBuilderWindowEnabled = true;
