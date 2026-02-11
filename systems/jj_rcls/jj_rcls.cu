978e850ed926e5b8e38d5b8107f3a43d47ed4c87
TheSnowMan <134663057+thesnowbrah@users.noreply.github.com>
2026-02-11 16:57:13 +0300

    optimization for float/double

--- systems/jj_rcls/jj_rcls.cu
+++ systems/jj_rcls/jj_rcls.cu
@@ -40,82 +40,82 @@ __host__ __device__ __forceinline__ void finiteDifferenceScheme_(name)(numb* cur
 {
     ifMETHOD(P(method), ExplicitEuler)
     {
-        Vnext(x1) = fmodf(V(x1) + H * V(x2), 2.0f * 3.141592f);
-        Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x2) = V(x2) + H * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
-        Vnext(x3) = V(x3) + H * (1.0f / P(betaL)) * (V(x2) - V(x3));
+        Vnext(x1) = fmod(V(x1) + H * V(x2), (numb)2.0 * (numb)3.141592);
+        Vnext(sin_x1) = sin(Vnext(x1));
+        Vnext(x2) = V(x2) + H * ((numb)1.0 / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sin(V(x1)) - V(x3));
+        Vnext(x3) = V(x3) + H * ((numb)1.0 / P(betaL)) * (V(x2) - V(x3));
     }
 
     ifMETHOD(P(method), SemiExplicitEuler)
     {
-        Vnext(x1) = fmodf(V(x1) + H * V(x2), 2.0f * 3.141592f);
-        Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x3) = V(x3) + H * (1.0f / P(betaL)) * (V(x2) - V(x3));
-        Vnext(x2) = V(x2) + H * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(Vnext(x1)) - Vnext(x3));
+        Vnext(x1) = fmod(V(x1) + H * V(x2), (numb)2.0 * (numb)3.141592);
+        Vnext(sin_x1) = sin(Vnext(x1));
+        Vnext(x3) = V(x3) + H * ((numb)1.0 / P(betaL)) * (V(x2) - V(x3));
+        Vnext(x2) = V(x2) + H * ((numb)1.0 / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sin(Vnext(x1)) - Vnext(x3));
     }
 
     ifMETHOD(P(method), ExplicitMidpoint)
     {
-        numb x1mp = fmodf(V(x1) + H * 0.5 * V(x2), 2.0f * 3.141592f);
-        numb x2mp = V(x2) + H * 0.5 * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
-        numb x3mp = V(x3) + H * 0.5 * (1.0f / P(betaL)) * (V(x2) - V(x3));
+        numb x1mp = fmod(V(x1) + H * (numb)0.5 * V(x2), (numb)2.0 * (numb)3.141592);
+        numb x2mp = V(x2) + H * (numb)0.5 * ((numb)1.0 / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sin(V(x1)) - V(x3));
+        numb x3mp = V(x3) + H * (numb)0.5 * ((numb)1.0 / P(betaL)) * (V(x2) - V(x3));
 
-        Vnext(x1) = fmodf(V(x1) + H * x2mp, 2.0f * 3.141592f);
-        Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x2) = V(x2) + H * (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
-        Vnext(x3) = V(x3) + H * (1.0f / P(betaL)) * (x2mp - x3mp);
+        Vnext(x1) = fmod(V(x1) + H * x2mp, (numb)2.0 * (numb)3.141592);
+        Vnext(sin_x1) = sin(Vnext(x1));
+        Vnext(x2) = V(x2) + H * ((numb)1.0 / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sin(x1mp) - x3mp);
+        Vnext(x3) = V(x3) + H * ((numb)1.0 / P(betaL)) * (x2mp - x3mp);
     }
 
     ifMETHOD(P(method), ExplicitRungeKutta4)
     {
         numb kx11 = V(x2);
-        numb kx21 = (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(V(x1)) - V(x3));
-        numb kx31 = (1.0f / P(betaL)) * (V(x2) - V(x3));
+        numb kx21 = ((numb)1.0 / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sin(V(x1)) - V(x3));
+        numb kx31 = ((numb)1.0 / P(betaL)) * (V(x2) - V(x3));
 
-        numb x1mp = fmodf(V(x1) + H * 0.5 * kx11, 2.0f * 3.141592f);
-        numb x2mp = V(x2) + H * 0.5 * kx21;
-        numb x3mp = V(x3) + H * 0.5 * kx31;
+        numb x1mp = fmod(V(x1) + H * (numb)0.5 * kx11, (numb)2.0 * (numb)3.141592);
+        numb x2mp = V(x2) + H * (numb)0.5 * kx21;
+        numb x3mp = V(x3) + H * (numb)0.5 * kx31;
 
         numb kx12 = x2mp;
-        numb kx22 = (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
-        numb kx32 = (1.0f / P(betaL)) * (x2mp - x3mp);
+        numb kx22 = ((numb)1.0 / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sin(x1mp) - x3mp);
+        numb kx32 = ((numb)1.0 / P(betaL)) * (x2mp - x3mp);
 
-        x1mp = fmodf(V(x1) + H * 0.5 * kx12, 2.0f * 3.141592f);
-        x2mp = V(x2) + H * 0.5 * kx22;
-        x3mp = V(x3) + H * 0.5 * kx32;
+        x1mp = fmod(V(x1) + H * (numb)0.5 * kx12, (numb)2.0 * (numb)3.141592);
+        x2mp = V(x2) + H * (numb)0.5 * kx22;
+        x3mp = V(x3) + H * (numb)0.5 * kx32;
 
         numb kx13 = x2mp;
-        numb kx23 = (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
-        numb kx33 = (1.0f / P(betaL)) * (x2mp - x3mp);
+        numb kx23 = ((numb)1.0 / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sin(x1mp) - x3mp);
+        numb kx33 = ((numb)1.0 / P(betaL)) * (x2mp - x3mp);
 
-        x1mp = fmodf(V(x1) + H * kx13, 2.0f * 3.141592f);
+        x1mp = fmod(V(x1) + H * kx13, (numb)2.0 * (numb)3.141592);
         x2mp = V(x2) + H * kx23;
         x3mp = V(x3) + H * kx33;
 
         numb kx14 = x2mp;
-        numb kx24 = (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
-        numb kx34 = (1.0f / P(betaL)) * (x2mp - x3mp);
+        numb kx24 = ((numb)1.0 / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sin(x1mp) - x3mp);
+        numb kx34 = ((numb)1.0 / P(betaL)) * (x2mp - x3mp);
 
-        Vnext(x1) = fmodf(V(x1) + H * (kx11 + 2 * kx12 + 2 * kx13 + kx14) / 6, 2.0f * 3.141592f);
-        Vnext(sin_x1) = sinf(Vnext(x1));
-        Vnext(x2) = V(x2) + H * (kx21 + 2 * kx22 + 2 * kx23 + kx24) / 6;
-        Vnext(x3) = V(x3) + H * (kx31 + 2 * kx32 + 2 * kx33 + kx34) / 6;
+        Vnext(x1) = fmod(V(x1) + H * (kx11 + (numb)2.0 * kx12 + (numb)2.0 * kx13 + kx14) / (numb)6.0, (numb)2.0 * (numb)3.141592);
+        Vnext(sin_x1) = sin(Vnext(x1));
+        Vnext(x2) = V(x2) + H * (kx21 + (numb)2.0 * kx22 + (numb)2.0 * kx23 + kx24) / (numb)6.0;
+        Vnext(x3) = V(x3) + H * (kx31 + (numb)2.0 * kx32 + (numb)2.0 * kx33 + kx34) / (numb)6.0;
     }
 
     ifMETHOD(P(method), VariableSymmetryCD)
     {
-        numb h1 = 0.5 * H - P(symmetry);
-        numb h2 = 0.5 * H + P(symmetry);
+        numb h1 = (numb)0.5 * H - P(symmetry);
+        numb h2 = (numb)0.5 * H + P(symmetry);
 
-        numb x1mp = fmodf(V(x1) + h1 * V(x2), 2.0f * 3.141592f);
-        numb x3mp = V(x3) + h1 * (1.0f / P(betaL)) * (V(x2) - V(x3));
-        numb x2mp = V(x2) + h1 * (1.0f / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sinf(x1mp) - x3mp);
+        numb x1mp = fmod(V(x1) + h1 * V(x2), (numb)2.0 * (numb)3.141592);
+        numb x3mp = V(x3) + h1 * ((numb)1.0 / P(betaL)) * (V(x2) - V(x3));
+        numb x2mp = V(x2) + h1 * ((numb)1.0 / P(betaC)) * (P(i) - ((V(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * V(x2) - sin(x1mp) - x3mp);
 
-        Vnext(x2) = x2mp + h2 * (1.0f / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sinf(x1mp) - x3mp);
-        Vnext(x2) = x2mp + h2 * (1.0f / P(betaC)) * (P(i) - ((Vnext(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * Vnext(x2) - sinf(x1mp) - x3mp);
-        Vnext(x3) = (x3mp + h2 * (1.0f / P(betaL)) * Vnext(x2)) / (1.0f + h2 * (1.0f / P(betaL)));
-        Vnext(x1) = fmodf(x1mp + h2 * Vnext(x2), 2.0f * 3.141592f);
-        Vnext(sin_x1) = sinf(Vnext(x1));
+        Vnext(x2) = x2mp + h2 * ((numb)1.0 / P(betaC)) * (P(i) - ((x2mp > P(gThreshold)) ? P(Rn) : P(Rsg)) * x2mp - sin(x1mp) - x3mp);
+        Vnext(x2) = x2mp + h2 * ((numb)1.0 / P(betaC)) * (P(i) - ((Vnext(x2) > P(gThreshold)) ? P(Rn) : P(Rsg)) * Vnext(x2) - sin(x1mp) - x3mp);
+        Vnext(x3) = (x3mp + h2 * ((numb)1.0 / P(betaL)) * Vnext(x2)) / ((numb)1.0 + h2 * ((numb)1.0 / P(betaL)));
+        Vnext(x1) = fmod(x1mp + h2 * Vnext(x2), (numb)2.0 * (numb)3.141592);
+        Vnext(sin_x1) = sin(Vnext(x1));
     }
 }
 
