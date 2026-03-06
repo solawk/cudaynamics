c10de8845a12da4cf8aa809ac8c20bdd60cd89fe
TheSnowMan <134663057+thesnowbrah@users.noreply.github.com>
2026-03-06 11:03:21 +0300

    Fixed triangle waveform

--- systems/mishchenko/mishchenko.cu
+++ systems/mishchenko/mishchenko.cu
@@ -43,14 +43,14 @@ __host__ __device__ __forceinline__ void finiteDifferenceScheme_(name)(numb* cur
         numb gamp = P(Gammadc) + (fmod((V(t) - P(Gammadel)) > (numb)0.0 ? (V(t) - P(Gammadel)) : (P(Gammadf) / P(Gammafreq) + P(Gammadel) - V(t)), (numb)1.0 / P(Gammafreq)) < P(Gammadf) / P(Gammafreq) ? P(Gammaamp) : (numb)0.0);
         numb tmp = V(t) + H * (numb)0.5;
 
-        numb xmp = fmod(V(x) + H * (numb)0.5 * V(y), (numb)2.0 * (numb)3.141592);
+        numb xmp = fmod(V(x) + H * (numb)0.5 * V(y), (numb)2.0 * (numb)3.141592653589793);
         numb ymp = V(y) + H * (numb)0.5 * V(z);
         numb zmp = V(z) + H * (numb)0.5 * (((numb)1.0 / (P(eps1) * P(eps2))) * (gamp - (P(eps1) + P(eps2)) * V(z) - ((numb)1.0 - (P(eps1) * cos(V(x)))) * V(y)));
 
         Vnext(gamma) = P(Gammadc) + (fmod((tmp - P(Gammadel)) > (numb)0.0 ? (tmp - P(Gammadel)) : (P(Gammadf) / P(Gammafreq) + P(Gammadel) - tmp), (numb)1.0 / P(Gammafreq)) < P(Gammadf) / P(Gammafreq) ? P(Gammaamp) : (numb)0.0);
         Vnext(t) = V(t) + H;
 
-        Vnext(x) = fmod(V(x) + H * ymp, (numb)2.0 * (numb)3.141592);
+        Vnext(x) = fmod(V(x) + H * ymp, (numb)2.0 * (numb)3.141592653589793);
         Vnext(cosx) = cos(Vnext(x));
         Vnext(y) = V(y) + H * zmp;
         Vnext(z) = V(z) + H * (numb)0.5 * (((numb)1.0 / (P(eps1) * P(eps2))) * (Vnext(gamma) - (P(eps1) + P(eps2)) * zmp - ((numb)1.0 - (P(eps1) * cos(xmp))) * ymp));
@@ -58,17 +58,17 @@ __host__ __device__ __forceinline__ void finiteDifferenceScheme_(name)(numb* cur
 
     ifSIGNAL(P(signal), sine)
     {
-        numb gamp = P(Gammadc) + P(Gammaamp) * sin((numb)2.0 * (numb)3.141592 * P(Gammafreq) * (V(t) - P(Gammadel)));
+        numb gamp = P(Gammadc) + P(Gammaamp) * sin((numb)2.0 * (numb)3.141592653589793 * P(Gammafreq) * (V(t) - P(Gammadel)));
         numb tmp = V(t) + H * (numb)0.5;
 
-        numb xmp = fmod(V(x) + H * (numb)0.5 * V(y), (numb)2.0 * (numb)3.141592);
+        numb xmp = fmod(V(x) + H * (numb)0.5 * V(y), (numb)2.0 * (numb)3.141592653589793);
         numb ymp = V(y) + H * (numb)0.5 * V(z);
         numb zmp = V(z) + H * (numb)0.5 * (((numb)1.0 / (P(eps1) * P(eps2))) * (gamp - (P(eps1) + P(eps2)) * V(z) - ((numb)1.0 - (P(eps1) * cos(V(x)))) * V(y)));
 
-        Vnext(gamma) = P(Gammadc) + P(Gammaamp) * sin((numb)2.0 * (numb)3.141592 * P(Gammafreq) * (tmp - P(Gammadel)));
+        Vnext(gamma) = P(Gammadc) + P(Gammaamp) * sin((numb)2.0 * (numb)3.141592653589793 * P(Gammafreq) * (tmp - P(Gammadel)));
         Vnext(t) = V(t) + H;
 
-        Vnext(x) = fmod(V(x) + H * ymp, (numb)2.0 * (numb)3.141592);
+        Vnext(x) = fmod(V(x) + H * ymp, (numb)2.0 * (numb)3.141592653589793);
         Vnext(cosx) = cos(Vnext(x));
         Vnext(y) = V(y) + H * zmp;
         Vnext(z) = V(z) + H * (numb)0.5 * (((numb)1.0 / (P(eps1) * P(eps2))) * (Vnext(gamma) - (P(eps1) + P(eps2)) * zmp - ((numb)1.0 - (P(eps1) * cos(xmp))) * ymp));
@@ -76,17 +76,17 @@ __host__ __device__ __forceinline__ void finiteDifferenceScheme_(name)(numb* cur
 
     ifSIGNAL(P(signal), triangle)
     {
-        numb gamp = P(Gammadc) + P(Gammaamp) * (((numb)4.0 * P(Gammafreq) * (V(t) - P(Gammadel)) - (numb)2.0 * floor((((numb)4.0 * P(Gammafreq) * (V(t) - P(Gammadel)) + (numb)1.0) / (numb)2.0))) * pow((numb)-1.0, floor((((numb)4.0 * P(Gammafreq) * (V(t) - P(Gammadel)) + (numb)1.0) / (numb)2.0))));
+        numb gamp = P(Gammadc) + P(Gammaamp) * (((numb)4.0 * P(Gammafreq) * (V(t) - P(Gammadel)) - (numb)2.0 * floor(((numb)4.0 * P(Gammafreq) * (V(t) - P(Gammadel)) + (numb)1.0) / (numb)2.0)) * ((int)floor(((numb)4.0 * P(Gammafreq) * (V(t) - P(Gammadel)) + (numb)1.0) / (numb)2.0) % 2 == 0 ? (numb)1.0 : (numb)-1.0));
         numb tmp = V(t) + H * (numb)0.5;
 
-        numb xmp = fmod(V(x) + H * (numb)0.5 * V(y), (numb)2.0 * (numb)3.141592);
+        numb xmp = fmod(V(x) + H * (numb)0.5 * V(y), (numb)2.0 * (numb)3.141592653589793);
         numb ymp = V(y) + H * (numb)0.5 * V(z);
         numb zmp = V(z) + H * (numb)0.5 * (((numb)1.0 / (P(eps1) * P(eps2))) * (gamp - (P(eps1) + P(eps2)) * V(z) - ((numb)1.0 - (P(eps1) * cos(V(x)))) * V(y)));
 
-        Vnext(gamma) = P(Gammadc) + P(Gammaamp) * (((numb)4.0 * P(Gammafreq) * (tmp - P(Gammadel)) - (numb)2.0 * floor((((numb)4.0 * P(Gammafreq) * (tmp - P(Gammadel)) + (numb)1.0) / (numb)2.0))) * pow((numb)-1.0, floor((((numb)4.0 * P(Gammafreq) * (tmp - P(Gammadel)) + (numb)1.0) / (numb)2.0))));
+        Vnext(gamma) = P(Gammadc) + P(Gammaamp) * (((numb)4.0 * P(Gammafreq) * (tmp - P(Gammadel)) - (numb)2.0 * floor(((numb)4.0 * P(Gammafreq) * (tmp - P(Gammadel)) + (numb)1.0) / (numb)2.0)) * ((int)floor(((numb)4.0 * P(Gammafreq) * (tmp - P(Gammadel)) + (numb)1.0) / (numb)2.0) % 2 == 0 ? (numb)1.0 : (numb)-1.0));
         Vnext(t) = V(t) + H;
 
-        Vnext(x) = fmod(V(x) + H * ymp, (numb)2.0 * (numb)3.141592);
+        Vnext(x) = fmod(V(x) + H * ymp, (numb)2.0 * (numb)3.141592653589793);
         Vnext(cosx) = cos(Vnext(x));
         Vnext(y) = V(y) + H * zmp;
         Vnext(z) = V(z) + H * (numb)0.5 * (((numb)1.0 / (P(eps1) * P(eps2))) * (Vnext(gamma) - (P(eps1) + P(eps2)) * zmp - ((numb)1.0 - (P(eps1) * cos(xmp))) * ymp));
