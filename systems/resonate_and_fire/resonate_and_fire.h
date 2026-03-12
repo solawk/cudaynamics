f423257b462cf35f7b464044e0068dcf3af93fea
Alexander <fulmenius@yandex.ru>
2026-03-12 14:33:13 +0300

    hh dopri uninitialized

--- systems/resonate_and_fire/resonate_and_fire.h
+++ systems/resonate_and_fire/resonate_and_fire.h
@@ -3,7 +3,6 @@
 
 #define name resonate_and_fire
 
-
 const int THREADS_PER_BLOCK_(name) = 64;
 
 __global__ void gpu_wrapper_(name)(Computation* data, uint64_t variation);
@@ -12,4 +11,4 @@ __host__ __device__ void kernelProgram_(name)(Computation* data, uint64_t variat
 
 __host__ __device__ __forceinline__ void finiteDifferenceScheme_(name)(numb* currentV, numb* nextV, numb* parameters);
 
-#undef name;
+#undef name
