ce3e09998d3e6d445fd37bb1aa94810f5807898e
Alexander <fulmenius@yandex.ru>
2026-03-13 14:14:44 +0300

    easier numb switch

--- numb.h
+++ numb.h
@@ -1,6 +1,15 @@
 #pragma once
 
-#define numb double
+// Put 0 for float, 1 for double
+#define USE_DOUBLE_PRECISION 0
+
+#if !USE_DOUBLE_PRECISION
+	#define numb float
+#else
+	#define numb double
+#endif
+
+#undef USE_DOUBLE_PRECISION
 
 #pragma warning(disable:4305)
 #pragma warning(disable:4244)
