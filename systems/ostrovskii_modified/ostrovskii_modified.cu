38a3a2cfd970b9731637da4a673509c1a9bcc3d5
Alexander <fulmenius@yandex.ru>
2026-04-10 22:27:48 +0300

    om optimizations

--- systems/ostrovskii_modified/ostrovskii_modified.cu
+++ systems/ostrovskii_modified/ostrovskii_modified.cu
@@ -46,50 +46,51 @@ __host__ __device__ void kernelProgram_(name)(Computation* data, uint64_t variat
 	AnalysisLobby(data, &finiteDifferenceScheme_(name), variation);
 }
 
-template <typename T> __host__ __device__ __forceinline__ T signal_square(T t, const numb* parameters)
+__host__ __device__ __forceinline__ numb signal_square(numb t, const numb* parameters)
 {
-    const T idc = P(Idc);
-    const T idel = P(Idel);
-    const T idf = P(Idf);
-    const T ifreq = P(Ifreq);
-    const T iamp = P(Iamp);
+    const numb idc = P(Idc);
+    const numb idel = P(Idel);
+    const numb idf = P(Idf);
+    const numb ifreq = P(Ifreq);
+    const numb iamp = P(Iamp);
 
-    const T period = (T)1.0 / ifreq;
-    const T duty = idf / ifreq;
+    const numb period = (numb)1.0 / ifreq;
+    const numb duty = idf / ifreq;
 
-    const T tt = (t - idel) > (T)0.0 ? (t - idel) : (duty + idel - t);
-    const T ph = fmod(tt, period);
+    const numb tt = (t - idel) > (numb)0.0 ? (t - idel) : (duty + idel - t);
+    const numb ph = fmod(tt, period);
 
-    return idc + (ph < duty ? iamp : (T)0.0);
+    return idc + (ph < duty ? iamp : (numb)0.0);
 }
 
-template <typename T> __host__ __device__ __forceinline__ T signal_sine(T t, const numb* parameters)
+__host__ __device__ __forceinline__ numb signal_sine(numb t, const numb* parameters)
 {
-    const T pi2 = (T)6.283185307179586476925286766559;
+    const numb pi2 = (numb)6.283185307179586476925286766559;
+
     return P(Idc) + P(Iamp) * sin(pi2 * P(Ifreq) * (t - P(Idel)));
 }
 
-template <typename T> __host__ __device__ __forceinline__ T signal_triangle(T t, const numb* parameters)
+__host__ __device__ __forceinline__ numb signal_triangle(numb t, const numb* parameters)
 {
-    const T q = (T)4.0 * P(Ifreq) * (t - P(Idel));
-    const T k = floor((q + (T)1.0) / (T)2.0);
-    const T s = (((int)k) % 2 == 0) ? (T)1.0 : (T)-1.0;
-    return P(Idc) + P(Iamp) * ((q - (T)2.0 * k) * s);
+    numb q = (numb)4.0 * P(Ifreq) * (t - P(Idel));
+    numb k = floor((q + (numb)1.0) / (numb)2.0);
+    numb s = (((int)k) % 2 == 0) ? (numb)1.0 : (numb)-1.0;
+    return P(Idc) + P(Iamp) * ((q - (numb)2.0 * k) * s);
 }
 
-template <typename T> __host__ __device__ __forceinline__ T memristor_current(T v, T x, const numb* parameters)
+__host__ __device__ __forceinline__ numb memristor_current(numb v, numb x, const numb* parameters)
 {
-    const T du = -v + P(Uvm);
-    return (du > (T)0.0)
+    numb du = -v + P(Uvm);
+    return (du > (numb)0.0)
         ? du * x / P(Ron_p) + P(Ilk)
         : du * x / P(Ron_n) - P(Ilk);
 }
 
-template <typename T> __host__ __device__ __forceinline__ T diode_current(T v, const numb* parameters)
+__host__ __device__ __forceinline__ numb diode_current(numb v, const numb* parameters)
 {
-    const T vu = v + P(Utd);
-    const T invVt = (T)1.0 / P(Vt);
-    const T invVp = (T)1.0 / P(Vp);
+    numb vu = v + P(Utd);
+    numb invVt = (numb)1.0 / P(Vt);
+    numb invVp = (numb)1.0 / P(Vp);
 
     return
         P(Is) * (exp(vu * invVt) - exp(-vu * invVt)) +
@@ -97,46 +98,45 @@ template <typename T> __host__ __device__ __forceinline__ T diode_current(T v, c
         P(Iv) * (atan(P(D) * (vu - P(E))) + atan(P(D) * (vu + P(E))));
 }
 
-template <typename T> __host__ __device__ __forceinline__ T x_rhs(T v, T x, const numb* parameters)
+__host__ __device__ __forceinline__ numb x_rhs(numb v, numb x, const numb* parameters)
 {
-    const T one = (T)1.0;
-    const T xm1 = one - x;
-    const T u = -v + P(Uvm);
+    const numb one = (numb)1.0;
+    const numb xm1 = one - x;
 
-    const T xp = (P(Vh_p) - P(Vth_p)) * x + P(Vth_p);
-    const T xn = (P(Vh_n) - P(Vth_n)) * x + P(Vth_n);
+    numb Xp__sigS = (P(Vh_p) - P(Vth_p)) * x + P(Vth_p);
+    numb Xn__sigR = (P(Vh_n) - P(Vth_n)) * x + P(Vth_n);
 
-    const T dup = u - xp;
-    const T dun = u - xn;
+    numb Vndr = (P(Uvm) - v - Xp__sigS) * (P(Uvm) - v - Xn__sigR);
 
-    const T sig_s = one / (one + exp(-(dup * dun) / (P(Vs) * P(Vs))));
-    const T sig_r = one / (one + exp(-(dup * dun) / (P(Vr) * P(Vr))));
+    Xp__sigS = one / (one + exp(-Vndr / (P(Vs) * P(Vs))));
+    Xn__sigR = one / (one + exp(-Vndr / (P(Vr) * P(Vr))));
 
-    const T ax = P(A) * x;
-    const T axm = P(A) * xm1;
+    numb ax = P(A) * x;
+    numb axm = P(A) * xm1;
 
-    const T f_set =
+    numb f_set =
         (one - exp(-(ax + P(Ds)))) * xm1 +
         x * (one - exp(-axm));
 
-    const T f_reset =
+    numb f_reset =
         (one - exp(-ax)) * xm1 +
         x * (one - exp(-(axm + P(Dr))));
 
-    return (sig_s * f_set) / P(tau_s)
-        - ((one - sig_r) * f_reset) / P(tau_r);
+    return (Xp__sigS * f_set) / P(tau_s)
+        - ((one - Xn__sigR) * f_reset) / P(tau_r);
 }
 
-template <typename TSignal> __host__ __device__ __forceinline__ void rk4_scheme(numb* currentV, numb* nextV, numb* parameters, TSignal signal_fn)
+template <typename InputSignal> __host__ __device__ __forceinline__ void rk4_scheme(numb* currentV, numb* nextV, numb* parameters, InputSignal signal_fn)
 {
-    const numb v0 = V(v);
-    const numb x0 = V(x);
-    const numb t0 = V(t);
-
     const numb h = H;
     const numb h1 = (numb)0.5 * h;
     const numb h6 = h / (numb)6.0;
-    const numb invC = (numb)1.0 / P(C);
+
+    numb v0 = V(v);
+    numb x0 = V(x);
+    numb t0 = V(t);
+
+    numb invC = (numb)1.0 / P(C);
 
     numb sv, sx;
     numb vmp, xmp, tmp;
@@ -145,11 +145,11 @@ template <typename TSignal> __host__ __device__ __forceinline__ void rk4_scheme(
     // k1
     {
         const numb imp = signal_fn(t0, parameters);
-        const numb Id = diode_current<numb>(v0, parameters);
-        const numb Im = memristor_current<numb>(v0, x0, parameters);
+        const numb Id = diode_current(v0, parameters);
+        const numb Im = memristor_current(v0, x0, parameters);
 
         kv = (imp + Im - Id) * invC;
-        kx = x_rhs<numb>(v0, x0, parameters);
+        kx = x_rhs(v0, x0, parameters);
 
         sv = kv;
         sx = kx;
@@ -161,11 +161,11 @@ template <typename TSignal> __host__ __device__ __forceinline__ void rk4_scheme(
     // k2
     {
         const numb imp = signal_fn(tmp, parameters);
-        const numb Id = diode_current<numb>(vmp, parameters);
-        const numb Im = memristor_current<numb>(vmp, xmp, parameters);
+        const numb Id = diode_current(vmp, parameters);
+        const numb Im = memristor_current(vmp, xmp, parameters);
 
         kv = (imp + Im - Id) * invC;
-        kx = x_rhs<numb>(vmp, xmp, parameters);
+        kx = x_rhs(vmp, xmp, parameters);
 
         sv += (numb)2.0 * kv;
         sx += (numb)2.0 * kx;
@@ -177,11 +177,11 @@ template <typename TSignal> __host__ __device__ __forceinline__ void rk4_scheme(
     // k3
     {
         const numb imp = signal_fn(tmp, parameters);
-        const numb Id = diode_current<numb>(vmp, parameters);
-        const numb Im = memristor_current<numb>(vmp, xmp, parameters);
+        const numb Id = diode_current(vmp, parameters);
+        const numb Im = memristor_current(vmp, xmp, parameters);
 
         kv = (imp + Im - Id) * invC;
-        kx = x_rhs<numb>(vmp, xmp, parameters);
+        kx = x_rhs(vmp, xmp, parameters);
 
         sv += (numb)2.0 * kv;
         sx += (numb)2.0 * kx;
@@ -193,11 +193,11 @@ template <typename TSignal> __host__ __device__ __forceinline__ void rk4_scheme(
     // k4
     {
         const numb imp = signal_fn(tmp, parameters);
-        const numb Id = diode_current<numb>(vmp, parameters);
-        const numb Im = memristor_current<numb>(vmp, xmp, parameters);
+        const numb Id = diode_current(vmp, parameters);
+        const numb Im = memristor_current(vmp, xmp, parameters);
 
         kv = (imp + Im - Id) * invC;
-        kx = x_rhs<numb>(vmp, xmp, parameters);
+        kx = x_rhs(vmp, xmp, parameters);
 
         sv += kv;
         sx += kx;
@@ -216,17 +216,17 @@ __host__ __device__ __forceinline__ void finiteDifferenceScheme_(name)(numb* cur
     {
         ifSIGNAL(P(signal), square)
         {
-            rk4_scheme(currentV, nextV, parameters, signal_square<numb>);
+            rk4_scheme(currentV, nextV, parameters, signal_square);
         }
 
         ifSIGNAL(P(signal), sine)
         {
-            rk4_scheme(currentV, nextV, parameters, signal_sine<numb>);
+            rk4_scheme(currentV, nextV, parameters, signal_sine);
         }
 
         ifSIGNAL(P(signal), triangle)
         {
-            rk4_scheme(currentV, nextV, parameters, signal_triangle<numb>);
+            rk4_scheme(currentV, nextV, parameters, signal_triangle);
         }
     }
 }
